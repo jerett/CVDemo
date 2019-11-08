c049c3fc337853574b2391a717bc532c50cd62d4
jerett <wj.jiang@outlook.com>
2019-11-08 09:58:25 +0800

    add slambook14 tes.

---
+++ slambook14/ch5/stereo_vision.cc
@@ -0,0 +1,94 @@
+//
+// Created by jerett on 2019/11/6.
+//
+#include <iostream>
+#include <vector>
+#include <thread>
+
+#include <Eigen/Core>
+#include <opencv2/opencv.hpp>
+#include <pangolin/pangolin.h>
+
+void showPointCloud(const std::vector<Eigen::Vector4d, Eigen::aligned_allocator<Eigen::Vector4d>> &pointcloud);
+
+
+int main(int argc, char *argv[]) {
+    // 内参
+    double fx = 718.856, fy = 718.856, cx = 607.1928, cy = 185.2157;
+    // 基线
+    double b = 0.573;
+
+    cv::Mat left = cv::imread("left.png", cv::IMREAD_GRAYSCALE);
+    cv::Mat right = cv::imread("right.png", cv::IMREAD_GRAYSCALE);
+
+    auto sgbm = cv::StereoSGBM::create(
+        0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);
+
+    cv::Mat disparity_sgbm, disparity;
+    sgbm->compute(left, right, disparity_sgbm);
+    disparity_sgbm.convertTo(disparity, CV_32F, 1.0f / 16);
+
+    cv::imshow("left", left);
+    cv::imshow("right", right);
+    cv::imshow("disparity", disparity/96);
+
+    std::vector<Eigen::Vector4d, Eigen::aligned_allocator<Eigen::Vector4d>> pointcloud;
+    for (int v = 0; v < left.rows; ++v) {
+        for (int u = 0; u < left.cols; ++u) {
+            auto pixel_disparity = disparity.at<float>(v, u);
+            if (pixel_disparity <= 0 || pixel_disparity >= 96) continue;
+
+            Eigen::Vector4d point(0, 0, 0, left.at<uchar>(v, u)/255.0);
+            double x = (u - cx) / fx;
+            double y = (v - cy) / fy;
+            double depth = fx * b / pixel_disparity;
+            point[0] = x * depth;
+            point[1] = y * depth;
+            point[2] = depth;
+
+            pointcloud.push_back(point);
+        }
+    }
+
+    cv::waitKey(0);
+    showPointCloud(pointcloud);
+    return 0;
+}
+
+void showPointCloud(const std::vector<Eigen::Vector4d, Eigen::aligned_allocator<Eigen::Vector4d>> &pointcloud) {
+    if (pointcloud.empty()) {
+        std::cerr << "Point cloud is empty!" << std::endl;
+        return;
+    }
+
+    pangolin::CreateWindowAndBind("Point Cloud Viewer", 1024, 768);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    pangolin::OpenGlRenderState s_cam(
+        pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+        pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
+    );
+
+    pangolin::View &d_cam = pangolin::CreateDisplay()
+        .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
+        .SetHandler(new pangolin::Handler3D(s_cam));
+
+    while (pangolin::ShouldQuit() == false) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        d_cam.Activate(s_cam);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+        glPointSize(2);
+        glBegin(GL_POINTS);
+        for (auto &p: pointcloud) {
+            glColor3f(p[3], p[3], p[3]);
+            glVertex3d(p[0], p[1], p[2]);
+        }
+        glEnd();
+        pangolin::FinishFrame();
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    }
+}
